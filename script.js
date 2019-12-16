360c11e4112f74b981d41f63fc4859567d653027
Johnny Sharp <johnny@johnnycsharp.com>
2019-12-16 10:43:26 -0500

    Minor adjustments for project responsiveness

--- script.js
+++ script.js
@@ -1,47 +1,61 @@
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+window.addEventListener('resize', () => {
+  viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+});
 
 function lazyGrocerBg() {
-  var x = document.getElementsByClassName('lazyGrocer');
-  var i;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.backgroundSize = "cover";
-    x[i].style.backgroundImage = "url('../images/lazyGrocer1.png')";
+  if (viewportWidth > 678) {
+    var x = document.getElementsByClassName('lazyGrocer');
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundSize = "cover";
+      x[i].style.backgroundImage = "url('../images/lazyGrocer1.png')";
+    }
   }
 }
 
 function reviewSiteBg() {
-  var x = document.getElementsByClassName('reviewSite');
-  var i;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.backgroundSize = "cover";
-    x[i].style.backgroundImage = "url('../images/reviewSite.png')";
+  if (viewportWidth > 678) {
+    var x = document.getElementsByClassName('reviewSite');
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundSize = "cover";
+      x[i].style.backgroundImage = "url('../images/reviewSite.png')";
+    }
   }
 }
 
 function virtualPetBg() {
-  var x = document.getElementsByClassName('virtualPet');
-  var i;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.backgroundSize = "cover";
-    x[i].style.backgroundImage = "url('../images/petShelter1.png')";
+  if (viewportWidth > 678) {
+    var x = document.getElementsByClassName('virtualPet');
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundSize = "cover";
+      x[i].style.backgroundImage = "url('../images/petShelter1.png')";
+    }
   }
 }
 
 function fortuneTellerBg() {
-  var x = document.getElementsByClassName('fortuneTeller');
-  var i;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.backgroundSize = "cover";
-    x[i].style.backgroundPosition = "center";
-    x[i].style.backgroundImage = "url('../images/fortuneTeller.png')";
+  if (viewportWidth > 678) {
+    var x = document.getElementsByClassName('fortuneTeller');
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundSize = "cover";
+      x[i].style.backgroundPosition = "center";
+      x[i].style.backgroundImage = "url('../images/fortuneTeller.png')";
+    }
   }
 }
 
-
 function returnBg() {
-  var x = document.getElementsByClassName('returnBg');
-  var i;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.backgroundImage = "none";
-    x[i].style.backgroundColor = "inherit";
+  if (viewportWidth > 678) {
+    var x = document.getElementsByClassName('returnBg');
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.backgroundImage = "none";
+      x[i].style.backgroundColor = "inherit";
+    }
   }
 }
