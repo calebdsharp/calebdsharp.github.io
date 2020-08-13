fb6470ee4c2ff8d33d7597b9b4538c0d9cd6a009
Caleb <sharpcaleb007@gmail.com>
2020-08-12 22:33:08 -0400

    Updates

---
+++ precache-manifest.9eba1912feb5bb49ed850ab133fc45c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466975d328355817211bceb8bcaf00a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4be16656a64805c72730",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f9b5cbe5f566a97916cb",
+    "url": "/static/css/main.8664ad50.chunk.css"
+  },
+  {
+    "revision": "4be16656a64805c72730",
+    "url": "/static/js/2.e032b210.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e032b210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b5cbe5f566a97916cb",
+    "url": "/static/js/main.2317e50a.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "2212003279e048b973e975ba0f26cbd3",
+    "url": "/static/media/portfolio-image.22120032.JPG"
+  }
+]);
