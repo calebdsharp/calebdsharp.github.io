ebf397f4834b67842f739b09f6de23beb950d633
Caleb <sharpcaleb007@gmail.com>
2020-08-29 14:27:30 -0400

    remove fetch from photos

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -11,42 +11,44 @@ class Photos extends Component {
     }
 
     componentDidMount() {
-        fetch('https://graph.instagram.com//me/media?fields=id,caption,media_url&access_token=IGQVJXb0RreTNGbEFjNk9aQWZAQYnlGV3NGc3RMX1NYYzhKcUJCRVBwWUVzc0xSc0t2VmU0T2tGdDRHTURXTkYtM0dtaVdCM2FocmpicTRvLTZAWci14OHlfRjVpUjNHQnhQZAzIxaDROMVJPQ2xDUEhkbAZDZDD')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                this.setState({
-                    photos: data.data,
-                    isLoaded: true
-                });
-            }).catch(err => console.log(err))
+        // fetch('https://graph.instagram.com//me/media?fields=id,caption,media_url&access_token=IGQVJXb0RreTNGbEFjNk9aQWZAQYnlGV3NGc3RMX1NYYzhKcUJCRVBwWUVzc0xSc0t2VmU0T2tGdDRHTURXTkYtM0dtaVdCM2FocmpicTRvLTZAWci14OHlfRjVpUjNHQnhQZAzIxaDROMVJPQ2xDUEhkbAZDZDD')
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         console.log(data);
+        //         this.setState({
+        //             photos: data.data,
+        //             isLoaded: true
+        //         });
+        //     }).catch(err => console.log(err))
     }
 
     render() {
         const { error, photos, isLoaded } = this.state;
 
-        const displayPhotos = this.state.photos.map((pic, i) => {
-            let caption = pic.caption;
-            return (
-                <li className="photos" key={pic.id}>
-                    <figure>
-                        <img className="pic" src={pic.media_url} width="300px" height="300px" />
-                        <figcaption>{caption}</figcaption>
-                    </figure>
-                </li>
-            );
-        });
+        // const displayPhotos = this.state.photos.map((pic, i) => {
+        //     let caption = pic.caption;
+        //     return (
+        //         <li className="photos" key={pic.id}>
+        //             <figure>
+        //                 <img className="pic" src={pic.media_url} width="300px" height="300px" />
+        //                 <figcaption>{caption}</figcaption>
+        //             </figure>
+        //         </li>
+        //     );
+        // });
 
-        if (error) {
-            return <div>Error: {error.message}</div>;
-        } else if (!isLoaded) {
-            return <div style={{ textAlign: 'center' }}>Loading...</div>;
-        }
+        // {displayPhotos} // use to display in return
+
+        // if (error) {
+        //     return <div>Error: {error.message}</div>;
+        // } else if (!isLoaded) {
+        //     return <div style={{ textAlign: 'center' }}>Loading...</div>;
+        // }
 
         return (
             <div>
                 <ul className="pic-list">
-                    {displayPhotos}
+                    Photos coming soon
                 </ul>
             </div>
         );
